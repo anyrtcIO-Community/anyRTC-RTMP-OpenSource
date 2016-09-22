1e36062f5fe3fb5a40b114f599448067f4758e6e
DyncEric <liutao6982@gmail.com>
2016-09-22 19:43:37 +0800

    1, Update Android so 2, Add Copyright

--- Prj-Android/app/src/main/java/org/webrtc/PercentFrameLayout.java
+++ Prj-Android/app/src/main/java/org/webrtc/PercentFrameLayout.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2015 The WebRTC Project Authors. All rights reserved.
+ *  Copyright 2015 The WebRTC@AnyRTC project Authors. All rights reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
